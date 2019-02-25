a94bd67c2e055f93242c3a72db0b9b2749452738
rob cabrera <robert1cabrera@gmail.com>
2019-02-25 01:59:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(you) {
-  return 'Happy holidays, you!'
-}
-function happyHolidaysTo(holiday, you) {
-  return 'Happy Independence Day', 'you!'
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
 }
